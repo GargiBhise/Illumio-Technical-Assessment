55be442d5213e96d950a24fd2e9d929ba39be9d8
GargiBhise <gargib.2496@gmail.com>
2024-09-26 11:52:22 -0700

    Converted Lookup Table to a Dictionary

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import re
 
-
 with open("lookup_table", "r") as f:
     lookup_table = [line.strip() for line in f.readlines()]
+lookup_table_dct = {}
+for entry in lookup_table[1:]:
+    entry = entry.split(",")
+    lookup_table_dct[entry[0]] = {entry[1]:entry[2]}
+
 
 with open("logs.log", "r") as f:
     logs = [line.strip() for line in f.readlines()]
@@ -15,5 +19,3 @@ def extract_port_from_log(log: str) -> int:
         return int(match.group(1))
     else:
         return -1
-
-
