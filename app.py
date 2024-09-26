06b577bb7c5659666c80116823bd5b43d9a7bace
GargiBhise <gargib.2496@gmail.com>
2024-09-26 11:43:39 -0700

    RegEx to extract port number

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import re
+
+
 with open("lookup_table", "r") as f:
     lookup_table = [line.strip() for line in f.readlines()]
 
 with open("logs.log", "r") as f:
     logs = [line.strip() for line in f.readlines()]
+
+
+def extract_port_from_log(log: str) -> int:
+    pattern = r'(?:\S+\s+){5}(\d+)'
+    match = re.search(pattern, log)
+    if match:
+        return int(match.group(1))
+    else:
+        return -1
 
 
