d88436e3e020e058193d3c054e0b3dea6d61a41b
GargiBhise <gargib.2496@gmail.com>
2024-09-26 12:07:57 -0700

    Mapping Each Log to Tags

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ with open("lookup_table", "r") as f:
 lookup_table_dct = {}
 for entry in lookup_table[1:]:
     entry = entry.split(",")
-    lookup_table_dct[entry[0]] = {entry[1]:entry[2]}
+    lookup_table_dct[int(entry[0])] = {entry[1]:entry[2]}
 
 
 with open("logs.log", "r") as f:
@@ -19,3 +19,13 @@ def extract_port_from_log(log: str) -> int:
         return int(match.group(1))
     else:
         return -1
+
+
+for log in logs:
+    port_number = extract_port_from_log(log)
+    if port_number in lookup_table_dct:
+        metadata = lookup_table_dct[port_number]
+        protocol = list(metadata)[0]
+        tag = lookup_table_dct[port_number][protocol]
+    else:
+        protocol, tag = "Unknown", "Untagged"
